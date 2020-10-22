126de5a2fdd322f45240592e7e67a2153e0fc45d
bayuik <bayuindrakusuma05@gmail.com>
2020-10-22 20:06:43 +0700

    create tugas11.js and completed task eleven

---
+++ tugas11.js
@@ -0,0 +1,8 @@
+// Teman teman buatlah sebuah perhitungan dengan penjumlahan (+) , perkalian (*) dan pembagian (/)
+// Gunakan lah 4 angka ini 2, 4, 5 dan 10 sehingga menghasilkan output 25
+
+let num = () => {
+    return 4 * 10 / 2 + 5;
+}
+
+console.log(num());
