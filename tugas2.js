9ab27ea24de3cce4115b9dd453d80471acfcc0b0
bayuik <bayuindrakusuma05@gmail.com>
2020-10-22 10:42:03 +0700

    create tugas2.js and completed task two

---
+++ tugas2.js
@@ -0,0 +1,15 @@
+// Buatlah 5 buah variabel dengan 5 tipe data yang berbeda number , string , boolean , null atau undifined dan array
+// Didalam variabel tersebut teman teman isikan nilai yang berbeda tiap variabelnya
+// Setelah itu teman teman tampilkan kelima variabel tersebut dengan console.log()
+
+let name = 'bayu';
+let age = 19;
+let is_male = true;
+let redmi = ['redmi note', 8, 4, 64];
+let car = null;
+
+console.log(name);
+console.log(age);
+console.log(is_male);
+console.log(redmi);
+console.log(car);
