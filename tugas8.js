bfa43d086b51095d8ce5b7854e52d8fb218bbece
bayuik <bayuindrakusuma05@gmail.com>
2020-10-22 17:19:22 +0700

    create tugas8 and completed task eight

---
+++ tugas8.js
@@ -0,0 +1,14 @@
+// Buatlah sebuah variabel object versi teman teman.
+// Tampilkan nilai awal dari masing masing object yang teman teman buat.
+// Setelah itu teman teman lakukan perubahan nilai pada salah satu object yang teman teman buat dan tampilkan kembali.
+
+let aang = {
+    name : 'aang',
+    age : 100,
+    power: 'airbender',
+    friends: ['sokka', 'katara', 'toph', 'zuko']
+}
+
+console.log(aang);
+aang.power = 'avatar(airbender, firebender, earthbender, waterbender';
+console.log(aang);
