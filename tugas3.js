c263cecf33097507ddb512276da106566e933006
bayuik <bayuindrakusuma05@gmail.com>
2020-10-22 10:59:26 +0700

    create tugas3.js and completed task three

---
+++ tugas3.js
@@ -0,0 +1,13 @@
+// Buatlah sebuah function yang mempunyai nilai balik atau return dan sebuah parameter / argument
+// Setelah itu teman teman kirimlah sebuah nilai kedalam function tersebut
+// Didalam function tersebut teman teman ubah nilai dari variabel yang dikirim lalu kembalikan dengan return
+// Setelah nilai dikembalikan , tampilkan kedua nilai tersebut sebelum dirubah didalam function dan sesudah
+
+let addNumber = (a, b) => {
+    return a + b;
+}
+
+console.log('sebelum ditambahkan:');
+console.log('5 dan 4');
+console.log('Setelah ditambahkan');
+console.log(addNumber(5, 4))
