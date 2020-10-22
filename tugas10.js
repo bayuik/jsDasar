ee901d607d2ed8f1f41c236a5279da7869d858f7
bayuik <bayuindrakusuma05@gmail.com>
2020-10-22 19:18:08 +0700

    create tugas10.js and completed task ten

---
+++ tugas10.js
@@ -0,0 +1,7 @@
+//Buatlah sebuah perulangan untuk menampilkan bilangan seperti ini : 1, 3, 6, 10, 15, 21, 28, 36, 45, 55
+
+let num = 0;
+for(let i = 1; i < 11; i++) {
+    num+=i;
+    console.log(num);
+};
